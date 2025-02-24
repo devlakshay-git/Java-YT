1ee86506a68274ca00f638395219a8867b2abfbf
devlakshay-git <dev.lakshaysharma@gmail.com>
2025-02-24 20:35:43 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+    public static void main(String[] args) {
+        // System.out.println("My first code");
+
+        byte a = 12;
+        // System.out.println(a);
+        System.out.println("Byte Min: " + Byte.MIN_VALUE);
+        System.out.println("Byte Max: " + Byte.MAX_VALUE);
+        System.out.println("Short Min: " + Short.MIN_VALUE);
+        System.out.println("Short Max: " + Short.MAX_VALUE);
+        System.out.println("Integer Min: " + Integer.MIN_VALUE);
+        System.out.println("Integer Max: " + Integer.MAX_VALUE);
+        System.out.println("Long Min: " + Long.MIN_VALUE);
+        System.out.println("Long Max: " + Long.MAX_VALUE);
+        System.out.println("Long Max: " + Long.MAX_VALUE);
+
+    }
+}
