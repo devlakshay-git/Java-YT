5498808dac637849dfeda92e19b0b9b8936eb354
Lakshay Sharma <dev.lakshaysharma@gmail.com>
2025-02-24 20:40:02 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -15,5 +15,7 @@ public class Test {
         System.out.println("Long Max: " + Long.MAX_VALUE);
         System.out.println("Long Max: " + Long.MAX_VALUE);
 
+        System.out.println("Long Max: " + Long.MAX_VALUE);
+
     }
 }
